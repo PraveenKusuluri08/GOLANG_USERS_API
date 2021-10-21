7c4101af0b6e2b482b595d3666f92888e2f60c35
PraveenKusuluri08 <paveenkusuluri07@gmail.com>
2021-10-21 22:13:51 +0530

    Create Users-within the slices

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/PraveenKusuluri
+
+go 1.17
+
+require (
+	github.com/gorilla/mux v1.8.0
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519
+)
---
+++ main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gorilla/mux"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	Name     string
+	Email    string
+	Password string
+	Mobile   string
+	Role     int
+	Modules  []string
+	UserId   string
+}
+
+var users []User
+
+func (user *User) isEmty() bool {
+	// return c.CourseId == "" && c.CourseName == ""
+	return user.Name == ""
+}
+func main() {
+	fmt.Println("API🚀")
+	r := mux.NewRouter()
+	r.HandleFunc("/", standardRoute)
+	r.HandleFunc("/user", userRoute_Create).Methods("POST")
+	listen := http.ListenAndServe(":5000", r)
+	log.Fatal(listen)
+}
+
+func standardRoute(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(`<h1>🐇 golang</h1>`))
+
+}
+
+func userRoute_Create(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	if r.Body == nil {
+		json.NewEncoder(w).Encode("Please Enter the course details")
+	}
+	var user User
+	_ = json.NewDecoder(r.Body).Decode(&user)
+
+	fmt.Println(user.isEmty())
+
+	if user.isEmty() {
+		json.NewEncoder(w).Encode("No data present ")
+		return
+	}
+	rand.Seed(time.Now().UnixNano())
+	user.UserId = strconv.Itoa(rand.Intn(100))
+
+	hash, _ := passwordHasher(user.Password)
+
+	user.Password = hash
+
+	users = append(users, user)
+	json.NewEncoder(w).Encode(user)
+}
+
+// func handleError(err error) {
+// 	if err == nil {
+// 		panic(err)
+// 	}
+// }
+
+func passwordHasher(password string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), 15)
+	return string(hash), err
+}
